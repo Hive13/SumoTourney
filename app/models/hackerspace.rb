1592c7300bdbd49de50875c4f6da0bead9d4f77e
zombieCraig <agent.craig@gmail.com>
2011-03-15 01:52:52 -0400

    Fixed a bug with joining hackerspaces Showing hackerspaces now lists members of that hackerspace

--- app/models/hackerspace.rb
+++ app/models/hackerspace.rb
@@ -2,4 +2,8 @@ class Hackerspace < ActiveRecord::Base
  has_attached_file :pic, :styles => { :thumb=>"75x75>", :small => "150x150>" }
 
  has_many :contenders
+
+ def members
+   return Contender.find(:all, :conditions => {:hackerspace_id => self.id})
+ end
 end
